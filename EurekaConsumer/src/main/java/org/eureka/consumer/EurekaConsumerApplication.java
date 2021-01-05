58b4d886201b235bbf866a37a35154b7862700e7
cartoon <cartoonyu3@gmail.com>
2021-01-05 20:51:57 +0800

    revamp eureka demo maven import

--- EurekaConsumer/src/main/java/org/eureka/consumer/EurekaConsumerApplication.java
+++ EurekaConsumer/src/main/java/org/eureka/consumer/EurekaConsumerApplication.java
@@ -2,11 +2,14 @@ package org.eureka.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EurekaConsumerApplication {
 
     public static void main(String[] args) {
